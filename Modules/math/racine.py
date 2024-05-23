3f8c9b9151688cb762334c3374cfbd30cafbc12c
Fostinos <kafostin@gmail.com>
2024-05-23 19:08:52 +0100

    Math racine carre

--- Modules/math/racine.py
+++ Modules/math/racine.py
@@ -1,8 +1,18 @@
 import math
 from math import sqrt as racine_carre
+
+PI = 3.14
+[x, y, z ] = [ "Orange", "Banana", "Cherry" ]
+
 def entree() -> float:
+    local_var = 5
+    print(PI)
     while True:
         # recuperer l'entrée de l'utilisateur
+        """
+            ceci est un commentaire
+            sur plusieurs lignes
+        """
         nombre = input("Entrez un nombre réel: ")
         # faire la conversion en nombre réel
         try:
@@ -14,7 +24,8 @@ def entree() -> float:
 
 
 nombre = entree()
-
+nombrE = 0
 print("La racine carrée de", nombre, "est", math.sqrt(nombre))
 print(f"La racine carrée de {nombre} est {racine_carre(nombre)}")
 
+print(PI)
